4cb0d68afae618fcfbefb56e3bf37f9724f52634
Mark Hale <mark.hale94@gmail.com>
2023-10-21 21:21:13 -0500

    add animation that plays when the user taps space to fly

--- plane_cb/plane_cb.cs
+++ plane_cb/plane_cb.cs
@@ -5,11 +5,12 @@ public partial class plane_cb : CharacterBody2D
 {
 	private float _gravity = 1900.0f;
 	private float _power = -400.0f;
-
+	private AnimationPlayer _animationPlayer;
 	private Vector2 _previousVelocity;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,8 +20,11 @@ public partial class plane_cb : CharacterBody2D
 
 	public override void _PhysicsProcess(double delta)
 	{
+		bool isFlyPressed = Input.IsActionJustPressed("fly");
 		_previousVelocity = Velocity;
-		var newVelocityY = Input.IsActionJustPressed("fly") ? _power : (_previousVelocity.Y + _gravity * (float)delta);
+		var newVelocityY = isFlyPressed ? _power : (_previousVelocity.Y + _gravity * (float)delta);
+		if( isFlyPressed )
+			_animationPlayer.Play("fly");
 		Velocity = new Vector2(_previousVelocity.X, newVelocityY);
 		MoveAndSlide();
 
