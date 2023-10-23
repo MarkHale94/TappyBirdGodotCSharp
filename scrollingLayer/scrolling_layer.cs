07baa67d3ce33efb3b32f52526f33527a759d4ed
Mark Hale <mark.hale94@gmail.com>
2023-10-22 22:41:08 -0500

    Added parallax bgs as well as updated some label contents

---
+++ scrollingLayer/scrolling_layer.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public partial class scrolling_layer : ParallaxLayer
+{
+	private Sprite2D _sprite;
+
+	[Export] private Texture2D _texture;
+	[Export] private float _scrollScale;
+	[Export] private float _textureX = 1920.0f;
+	[Export] private float _textureY = 1080.0f;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_sprite = GetNode<Sprite2D>("Sprite2D");
+		_scrollScale = MotionScale.X;
+		var scaleF = GetViewportRect().Size.Y / _textureY;
+		_sprite.Texture = _texture;
+		_sprite.Scale = new Vector2(scaleF, scaleF);
+		MotionMirroring = new Vector2(_textureX * scaleF, MotionMirroring.Y) ;
+	}
+
+}
---
+++ scrollingbg/scrolling_bg.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+
+public partial class scrolling_bg : ParallaxBackground
+{
+	private float _speed = 120.0f;
+	private Vector2 _previousOffset;
+	private StringName _gameOver = gameManager.SignalName.OnGameOver;
+
+	private gameManager _gameManager;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_gameManager = GetNode<gameManager>("/root/GameManager");
+		_gameManager.Connect(_gameOver, new Callable(this, _gameOver));
+
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		_previousOffset = ScrollOffset;
+		var newOffsetX = _previousOffset.X + _speed * (float)delta * -1;
+		ScrollOffset = new Vector2(newOffsetX, _previousOffset.Y);
+	}
+
+	private void OnGameOver()
+	{
+		SetProcess(false);
+	}
+}
