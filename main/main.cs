bfa05946092d979c634a62de8cb8430ab904e246
Mark Hale <mark.hale94@gmail.com>
2023-10-22 22:10:52 -0500

    add high score updating on the main screen

--- main/main.cs
+++ main/main.cs
@@ -4,9 +4,16 @@ using System;
 public partial class main : Control
 {
 	private StringName _fly ="fly";
+
+	private gameManager _gameManager;
+
+	private Label _highScore;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_gameManager = GetNode<gameManager>("/root/GameManager");
+		_highScore = GetNode<Label>("MC/HB/HighScoreLabel");
+		_highScore.Text = _gameManager.GetHighScore().ToString();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
